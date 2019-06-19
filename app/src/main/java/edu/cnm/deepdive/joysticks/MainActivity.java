efcc24fe117bd828da53f926ed4bb28bc192d5dc
Brian <babta29@gmail.com>
2019-06-19 13:13:21 -0600

    Initial commit with two full screen joysticks

---
+++ app/src/main/java/edu/cnm/deepdive/joysticks/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive.joysticks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements JoystickView.JoystickListener{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    JoystickView joystick = new JoystickView(this);
+    setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public void onJoystickMoved(float xPercent, float yPercent, int id) {
+    switch (id)
+    {
+      case R.id.joystickRight:
+        Log.d("Right Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+        break;
+      case R.id.joystickLeft:
+        Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+        break;
+    }
+  }
+}
